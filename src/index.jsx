86490919d6e24cdc9538ebc7f7d9977ba8ecb839
maigohara <maigohara-1103@outlook.jp>
2020-05-16 13:41:56 -0700

    changed index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import timeReducer from './components/store/reducers/timerReducer';
@@ -13,9 +13,9 @@ const store = createStore(timeReducer);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
